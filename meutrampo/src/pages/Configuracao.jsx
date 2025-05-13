e3dec25f4209a547ccbc596a165a3fc6640e2ca0
João Ricardo de Sousa Matos <joaor.matos@dinfor.uniceub>
2025-05-13 10:29:03 -0300

    tentando fazer alguma coisa

--- meutrampo/src/pages/Configuracao.jsx
+++ meutrampo/src/pages/Configuracao.jsx
@@ -1,41 +1,98 @@
-import React from 'react'
+import React, { useState, ReactProps, useEffect } from 'react'
+import { supabase } from "../assets/client"
 import '../styles.css';
-import plus from '../assets/Icon.png'
+import check from '../assets/Check.png'
 
 const Configuracao = ({token}) => {
+  const [novoLink, setNovoLink] = useState({link_title: "", link_url: ""});
+  const [links, setLinks] = useState([]);
+
+
+  const fetchLinks = async () => {
+    const { error, data } = await supabase.from("links").select("*")
+    .order("created_at", { ascending: true });
+
+    if (error) {
+      console.error("Error reading link: ", error.message);
+      return;
+    }
+
+    setLinks(data);
+
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("comando acionado");
+    e.preventDefautlt();
+
+    const {error} = await supabase.from("links").insert(novoLink).single()
+
+    if (error) {
+      console.error("Error adding link: ", error.message);
+    }
+
+    setNovoLink({ link_title: "", link_url: "" });
+  };
+
+  useEffect(() => {
+    fetchLinks();
+  }, []);
+
+  console.log('oi');
+  console.log(links);
+
   return (
     <div className='backgroundConfig'>
       
     <div className='containerPortfolio'>
         <div className='configPortfolioBackground'>
-          <image className='configPortfolioBanner'></image> 
+          <img className='configPortfolioBanner'></img> 
         </div>
         
         <h1 className='configPortfolioNome'>{token.user.user_metadata.fullName}</h1>
         <p className='configPortfolioBio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis aliquam nisl. Nullam pellentesque finibus diam, in feugiat orci consectetur interdum. Etiam blandit semper lorem nec auctor. In in enim in neque fringilla placerat. Suspendisse turpis dolor, sodales et maximus in, convallis in lectus. Aenean tristique massa nec ex placerat consequat. Vivamus scelerisque vehicula iaculis.</p>
 
+        {/* Configurar links do portfolio */}
+
+        <form className='containerConfigLink'>
+
+            <div className='configLinkComponent'>
+                
+                <input className='configLinkText' placeholder='Texto' type='text' name='linkTitle'
+                onChange={(e) => setNovoLink((prev) => ({...prev, link_title: e.target.value }))}
+                />
+
+                <input className='configLinkText' placeholder='Link' type='text' name='linkUrl'
+                onChange={(e) => setNovoLink((prev) => ({...prev, link_url: e.target.value }))}
+                />
+
+            </div>
+          
+            <div className='containerConfigLinkBtn'>
+
+              <button className='configAdicionarLink'>
+                <img src={check} alt="" onClick={handleSubmit}/>
+              </button>
+
+            </div>
+        </form>
+
+          {/* Configurar perfil de usuário/empresa */}
+
         <div className='containerConfigLink'>
             <div className='configLinkComponent'>
-            <label className='configLinkIcone'>
-                  <input type='file' accept='image/png, image/jpeg'/>
-                </label>
-                <input className='configLinkText' placeholder='Texto' type='text' name='linkTitle'/>
-                <input className='configLinkText' placeholder='Link' type='text' name='linkTitle'/>
+                <input className='configLinkText' placeholder='Nome ou Empresa' type='text' name='linkTitle'/>
             </div>
+            
             <form className='configLinkComponent'>
-                <label className='configLinkIcone'>
-                  <input type='file' accept='image/png, image/jpeg'/>
-                </label>
-                <input className='configLinkText' placeholder='Texto' type='text' name='linkTitle'/>
-                <input className='configLinkText' placeholder='Link' type='text' name='linkTitle'/>
+                <input className='configProfileBio' placeholder='Biografia' type='text' name='linkTitle'/>
             </form>
+
             <div className='containerConfigLinkBtn'>
             <button className='configAdicionarLink'>
-              <img src={plus} alt="" />
+              <img src={check} alt="" />
             </button>
-            <button className='configAdicionarLink'>
-              <img src={plus} alt="" />
-            </button>
+
             </div>
         </div>
      
--- meutrampo/src/pages/Portfolio.jsx
+++ meutrampo/src/pages/Portfolio.jsx
@@ -6,17 +6,17 @@ const Portfolio = ({token}) => {
     <div className='backgroundLogin'>
       
     <div className='containerPortfolio'>
-        <image className='portfolioBanner'></image>
+        <img className='portfolioBanner'></img>
         <h1 className='portfolioNome'>{token.user.user_metadata.fullName}</h1>
         <p className='portfolioBio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis aliquam nisl. Nullam pellentesque finibus diam, in feugiat orci consectetur interdum. Etiam blandit semper lorem nec auctor. In in enim in neque fringilla placerat. Suspendisse turpis dolor, sodales et maximus in, convallis in lectus. Aenean tristique massa nec ex placerat consequat. Vivamus scelerisque vehicula iaculis.</p>
 
         <div>
             <div className='linkComponent'>
-                <image className='linkIcone'></image>
+                <img className='linkIcone'></img>
                 <text className='linkButton'>Link1</text>
             </div>
             <div className='linkComponent'>
-                <image className='linkIcone'></image>
+                <img className='linkIcone'></img>
                 <text className='linkButton'>Link2</text>
             </div>
         </div>
