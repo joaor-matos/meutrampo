f93bc8f6ce78cf07ea4a0bf957fbb32cdc82d8fe
João Ricardo de Sousa Matos <joaor.matos@dinfor.uniceub>
2025-06-17 10:03:58 -0300

    cabou

--- meutrampo/src/pages/Teste.jsx
+++ meutrampo/src/pages/Teste.jsx
@@ -42,16 +42,15 @@ const Teste = () => {
                     <div className='backgroundLogin'>
 
                         <div className='containerPortfolio'>
-                            <img className='portfolioBanner'></img>
                             <h1 className='portfolioNome'>{userData.fullname}</h1>
                             <p className='portfolioBio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis aliquam nisl. Nullam pellentesque finibus diam, in feugiat orci consectetur interdum. Etiam blandit semper lorem nec auctor. In in enim in neque fringilla placerat. Suspendisse turpis dolor, sodales et maximus in, convallis in lectus. Aenean tristique massa nec ex placerat consequat. Vivamus scelerisque vehicula iaculis.</p>
                             <div>
                                 <div className='linkComponent'>
-                                    <img className='linkIcone'></img>
+
                                     <text className='linkButton'>Link1</text>
                                 </div>
                                 <div className='linkComponent'>
-                                    <img className='linkIcone'></img>
+
                                     <text className='linkButton'>Link2</text>
                                 </div>
                             </div>
