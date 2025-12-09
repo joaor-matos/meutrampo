1081f06fc5b0a14e9bb26dd355b926bc46c0f379
João Ricardo de Sousa Matos <joaor.matos@dinfor.uniceub>
2025-12-09 09:27:27 -0300

    acabou por agora

--- meutrampo/src/pages/LandingPage.jsx
+++ meutrampo/src/pages/LandingPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import logo from '../assets/logo.png';
+import logofooter from '../assets/logofooter.png';
+import footerlinks from '../assets/footerlinks.png';
 
 function LandingPage() {
   return (
@@ -10,8 +13,18 @@ function LandingPage() {
     <div>
         
     </div>
-    <footer>
+    <footer className='footerLP'>
+    <div>
+      <img src={logofooter} alt="" />
+      <img src={footerlinks} alt="" className=''/>
 
+    </div>
+    {/* <ul>
+      <li><a href="" className='redlinkfooterLP'>Sobre o produto</a></li>
+      <li><a href="" className='redlinkfooterLP'>Nossos clientes</a></li>
+      <li><a href="" className='redlinkfooterLP'>Crie seu perfil</a></li>
+      <li><a href="" className='redlinkfooterLP'>Sobre nós</a></li>
+    </ul> */}
     </footer>
     </body>
 
